ed37ba2989af767ac3a3d2dfd4e3173ca6e13e91
Evelyn <evewoowp@gmail.com>
2025-07-16 05:25:52 +0000

    upd

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def prediction():
 
 @app.route("/telegram",methods=["GET","POST"])
 def telegram():
-    domain_url = 'https://dsat-ft1-ipop.onrender.com'
+    domain_url = 'https://module5-1-8nxh.onrender.com'
 
     # The following line is used to delete the existing webhook URL for the Telegram bot
     delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
